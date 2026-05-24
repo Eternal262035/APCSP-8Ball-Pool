c9c91f6326f4229309a7a32c9e1a39fa870cb50f
Eternal262035 <170258372+Eternal262035@users.noreply.github.com>
2026-05-24 16:48:36 -0400

    working on conserving momentum in collisions

--- src/Game/Physics/Collision.ts
+++ src/Game/Physics/Collision.ts
@@ -1,5 +1,5 @@
 import { entityManager } from "../Entity/EntityManager.js";
-import Vector from "./Vector.js";
+import Vector, { VectorAbstract } from "./Vector.js";
 
 /** currently it's the stupidest algo under the sun to do this
  * spatial hashing is overkill for something like this, 
@@ -16,22 +16,26 @@ export function checkForCollisions() {
             if (dr < firstEntity.hitboxData.size + secondEntity.hitboxData.size) {
                 // console.log(`${firstEntity.id} <--> ${secondEntity.id}`);
                 // firstEntity.physicsData.velocity.add(Vector.fromPolar(dr, Vector.direction({x: firstEntity.positionData.x-secondEntity.positionData.x, y: firstEntity.positionData.y-secondEntity.positionData.y})));
-                const mom1 = firstEntity.physicsData.velocity.magnitude * firstEntity.physicsData.mass;
-                const mom2 = secondEntity.physicsData.velocity.magnitude * secondEntity.physicsData.mass;
                 
-                const angle = Math.atan2(
+                // momentum is a vector
+                const mom1:VectorAbstract = firstEntity.physicsData.velocity.scale(firstEntity.physicsData.mass);
+                const mom2:VectorAbstract = secondEntity.physicsData.velocity.scale(secondEntity.physicsData.mass);
+                
+                const aoc = Math.atan2( // angle between the centers of the two entities
                     firstEntity.positionData.y-secondEntity.positionData.y,
                     firstEntity.positionData.x-secondEntity.positionData.x
                 );
-                const scale = Math.cos(angle);
+                const a1 = firstEntity.physicsData.velocity.angle; // angle of the first object's velocity
+                const a2 = secondEntity.physicsData.velocity.angle; // angle of the second object's velocity 
 
+                const da1 = aoc-a1; // angle between line formed between their centers and the velocity vector
+                const da2 = aoc-a2; // angle between line formed between their centers and the velocity vector
 
-                const correctionVector = new Vector(
-                    firstEntity.positionData.x-secondEntity.positionData.x, 
-                    firstEntity.positionData.y-secondEntity.positionData.y, 
-                ).scale(0.02)
-                firstEntity.physicsData.velocity.add(correctionVector);
-                secondEntity.physicsData.velocity.subtract(correctionVector);
+                const netmx = mom1.x - mom2.x;
+                const netmy = mom1.y - mom2.y;
+
+                firstEntity.physicsData.velocity.add()
+
             }
         }
     }
